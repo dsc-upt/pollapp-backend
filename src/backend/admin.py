aa2166147bd6352094c3ad9fc9b125bb247af2a0
Sonia <tuculia_sonia@yahoo.com>
2021-10-23 16:18:48 +0300

    Created the Option Model

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ExampleModel
+from .models import ExampleModel, Option
 
 # Register your models here.
 
 admin.site.register(ExampleModel)
+admin.site.register(Option)
