515423d6a0baf06fe0c2a66038118e2be7d7c012
Robert Hevesi <roberthevesi09@gmail.com>
2021-10-26 11:46:31 +0300

    Added Poll Model

--- src/backend/admin.py
+++ src/backend/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import ExampleModel
+from .models import Poll
 
 # Register your models here.
 
 admin.site.register(ExampleModel)
+admin.site.register(Poll)
--- src/backend/models.py
+++ src/backend/models.py
@@ -6,3 +6,8 @@ class ExampleModel(models.Model):
     title = models.CharField(max_length = 64)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+
+class Poll(models.Model):
+    title = models.CharField(max_length = 100)
+    question = models.TextField(max_length = 500)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
