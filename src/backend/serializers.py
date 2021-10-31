455996bee5f972ed0c4a8add2dca9df1131da93a
Robert Hevesi <roberthevesi09@gmail.com>
2021-10-31 20:47:37 +0200

    Added Poll Serializer

--- src/backend/serializers.py
+++ src/backend/serializers.py
@@ -5,3 +5,8 @@ class ExampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExampleModel
         fields = ('id', 'title', 'content', 'date')
+
+class PollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PollModel
+        fields = ('id', 'title', 'question', 'creator')
