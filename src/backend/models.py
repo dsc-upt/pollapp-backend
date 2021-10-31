743fd9a7ddf5d9eb7084fc1b558aa94f5126328a
Lore <loredana.gostian@student.upt.ro>
2021-10-31 20:46:46 +0200

    Added Vote Serializer

--- src/backend/models.py
+++ src/backend/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class ExampleModel(models.Model):
     title = models.CharField(max_length=64)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+
 class Poll(models.Model):
     title = models.CharField(max_length = 100)
     question = models.TextField(max_length = 500)
     creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
 class Option(models.Model):
     content = models.TextField()
@@ -21,4 +24,4 @@ class Vote(models.Model):
     option = models.ForeignKey(Option, on_delete=models.CASCADE)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     voter = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now_add = True)
+    date = models.DateTimeField(auto_now_add=True)
--- src/backend/serializers.py
+++ src/backend/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from backend.models import ExampleModel
+from backend.models import ExampleModel, Vote
+
 
 class ExampleSerializer(serializers.ModelSerializer):
     class Meta:
         model = ExampleModel
         fields = ('id', 'title', 'content', 'date')
+
+
+class Vote(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('option', 'poll', 'voter', 'date')
